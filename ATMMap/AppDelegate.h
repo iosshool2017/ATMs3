6d57065b250c5a8d188c0189b3a48ce456bc2447
Admin <admin@Mac-Admin.local>
2017-05-10 19:47:00 +0300

    Initial Commit

---
+++ ATMMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ATMMap
+//
+//  Created by Admin on 10.05.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ATMMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ATMMap
+//
+//  Created by Admin on 10.05.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
